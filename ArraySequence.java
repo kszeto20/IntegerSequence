75a47fd37d57b9b62942b6190ccdc73bc3d6f44f
Kirsten Szeto <kszeto20@stuy.edu>
2020-12-08 12:54:16 -0500

    added all the interface methods and they're all tested.

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,4 +1,36 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
+  private int currentIndex;
+  private int[] data;
 
+  public ArraySequence (int[] other) {
+    data = other;
+    currentIndex = 0;
+  }
+
+  public void reset() {
+    currentIndex = 0;
+  }
+
+  public int length() {
+    return (data.length);
+  }
+
+  public boolean hasNext() {
+    if (currentIndex < data.length){
+      return true;
+    }
+    return false;
+  }
+
+  public int next() {
+    if (this.hasNext()) {
+      int oldCur = data[currentIndex];
+      currentIndex++;
+      return oldCur;
+    }
+    else {
+      throw new NoSuchElementException("No such element. You're the end of the range");
+    }
+  }
 }
--- IntSeqTester.java
+++ IntSeqTester.java
@@ -7,5 +7,14 @@ public class IntSeqTester {
           System.out.print( ", " );
     }
     System.out.println();
+
+
+    int[]nums = {1,3,5,0,-1,3,9};
+    IntegerSequence as = new ArraySequence(nums);
+    System.out.println("ArraySequence(array):");
+    while(as.hasNext()){
+      System.out.print(as.next()+", ");
+    }
+    System.out.println();
   }
 }
