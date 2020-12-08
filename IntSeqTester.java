f1effa806d75ed200ac53e4f80bfd2b013071896
Kirsten Szeto <kszeto20@stuy.edu>
2020-12-08 01:31:25 -0500

    intSeq interface tester

---
+++ IntSeqTester.java
@@ -0,0 +1,11 @@
+public class IntSeqTester {
+  public static void main (String[] args) {
+    IntegerSequence r = new Range(10,20);
+    while(r.hasNext()){
+      System.out.print(r.next());
+       if( r.hasNext() )
+          System.out.print( ", " );
+    }
+    System.out.println();
+  }
+}
